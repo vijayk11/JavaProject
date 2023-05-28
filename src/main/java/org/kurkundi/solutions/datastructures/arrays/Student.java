f4591a705082ad52bc0a44f200e22697ee609126
vijayk11 <kurkundi.vijay@gmail.com>
2023-05-28 14:20:13 +0530

    arrays

---
+++ src/main/java/org/kurkundi/solutions/datastructures/arrays/Student.java
@@ -0,0 +1,55 @@
+package org.kurkundi.solutions.datastructures.arrays;
+
+public class Student {
+    String name;
+    int[] marks;
+    public Student(){}
+    public Student(String name, int[] marks){
+        this.name =name;
+        this.marks =marks;
+    }
+    public int[] getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int[] marks) {
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalSumOfMarks(){
+        int sum=0;
+        for(int mark:marks){
+            sum=sum+mark;
+        }
+        return sum;
+    }
+
+    public int getMaximumMark(){
+        int max = Integer.MIN_VALUE;
+        for(int mark: marks){
+            if(max<mark){
+                max=mark;
+            }
+        }
+        return  max;
+    }
+
+    protected int getMinimumMark(){
+        int min = Integer.MAX_VALUE;
+        for(int mark: marks){
+            if(min>mark){
+                min = mark;
+            }
+        }
+        return min;
+    }
+
+}
---
+++ src/main/java/org/kurkundi/solutions/datastructures/arrays/StudentRunner.java
@@ -0,0 +1,19 @@
+package org.kurkundi.solutions.datastructures.arrays;
+
+
+import java.util.Arrays;
+
+public class StudentRunner {
+    public static void main(String[] args){
+        int[] marks = {90,95,100};
+        Student vijay = new Student("vijay",marks);
+
+        System.out.println(Arrays.toString(vijay.getMarks()));
+
+        System.out.println("Total Sum of marks "+vijay.getTotalSumOfMarks());
+        System.out.println("Maximum Mark obtained "+vijay.getMaximumMark());
+        System.out.println("Minimum Maximum obtained "+vijay.getMinimumMark());
+
+    }
+
+}
