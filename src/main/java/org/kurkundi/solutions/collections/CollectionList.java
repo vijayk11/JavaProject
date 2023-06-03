40fed67affdf2f9e807c431e797a132af654c770
vijayk11 <kurkundi.vijay@gmail.com>
2023-06-03 11:46:58 +0530

    collection

---
+++ src/main/java/org/kurkundi/solutions/collections/CollectionList.java
@@ -0,0 +1,11 @@
+package org.kurkundi.solutions.collections;
+
+import java.util.List;
+
+public class CollectionList {
+
+    public static void main(String[] args){
+        List<String> fruits = List.of("Apple","Mango");
+        System.out.println(fruits.size());
+    }
+}
