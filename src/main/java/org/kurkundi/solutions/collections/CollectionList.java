4028eb96e30e0c0319a72bd07e624f65b2a6c2d0
vijayk11 <kurkundi.vijay@gmail.com>
2023-06-03 12:46:37 +0530

    collection

--- src/main/java/org/kurkundi/solutions/collections/CollectionList.java
+++ src/main/java/org/kurkundi/solutions/collections/CollectionList.java
@@ -1,5 +1,6 @@
 package org.kurkundi.solutions.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionList {
@@ -7,5 +8,10 @@ public class CollectionList {
     public static void main(String[] args){
         List<String> fruits = List.of("Apple","Mango");
         System.out.println(fruits.size());
+        // fruits.add("fruit"); -> Exception bcz Immutable
+
+        List<String> fruitsArrayList = new ArrayList<>(fruits);
+        fruitsArrayList.add("Kiwi");
+        fruitsArrayList.forEach(f-> System.out.println(f.toUpperCase().concat(" is a fruit")) );
     }
 }
