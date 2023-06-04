3045a93e86c881dabb2f6c3384528a49ac360f7a
vijayk11 <kurkundi.vijay@gmail.com>
2023-06-04 21:49:52 +0530

    collection map

---
+++ src/main/java/org/kurkundi/solutions/collections/CollectionMap.java
@@ -0,0 +1,39 @@
+package org.kurkundi.solutions.collections;
+
+// Map doesnt implement Collection Interface
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CollectionMap {
+
+    public static void main(String[] args){
+        Map<String, Integer> map = Map.of("A",3,"B",5,"c",6);
+        System.out.println(map); //{B=5, A=3, c=6}
+        System.out.println(map.get("A") + " " + map.containsKey("B")+" "+map.containsValue(6));
+        //3 true true
+
+        Map<String,Integer> map1= new HashMap<>(map);
+        map1.put("F",11);
+        System.out.println(map1); // {A=3, B=5, c=6, F=11}
+
+        LinkedHashMap<String,Integer> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("A",25);
+        linkedHashMap.put("Z",50);
+        linkedHashMap.put("s",11);
+        System.out.println(linkedHashMap); //{A=25, Z=50, s=11}
+
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("A",25);
+        treeMap.put("Z",50);
+        treeMap.put("s",11);
+        System.out.println(treeMap);
+
+
+
+
+
+    }
+}
